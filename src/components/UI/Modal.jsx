351f87ec0ca2d60c8b3514b4c4061d81bea6c6c7
Pelin <108188410+PelinCinar@users.noreply.github.com>
2024-07-24 15:16:08 +0300

    click out side

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,62 +1,61 @@
 import { createPortal } from "react-dom";
 import PropTypes from "prop-types";
 
-
 function Modal({ setIsShowModal, title, desc }) {
   function handleClose() {
     setIsShowModal(false);
   }
 
   return createPortal(
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75">
-    <div className="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full">
-      <div className="flex items-center justify-between p-4 border-b border-gray-200">
-        <h2 className="text-lg font-semibold text-gray-900">{title}</h2>
-        <button
-          type="button"
-          className="text-gray-500 hover:text-gray-700"
-          onClick={handleClose}
-        >
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
+    <div className="fixed inset-0 flex items-center justify-center">
+      <div className="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full z-50">
+        <div className="flex items-center justify-between p-4 border-b border-gray-200">
+          <h2 className="text-lg font-semibold text-gray-900">{title}</h2>
+          <button type="button" className="text-gray-500 hover:text-gray-700">
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              ></path>
+            </svg>
+          </button>
+        </div>
+        <div className="p-4">
+          <p>{desc}</p>
+        </div>
+        <div className="px-4 py-3 border-t border-gray-200 bg-gray-50 sm:px-6 sm:flex sm:flex-row-reverse">
+          <button
+            type="button"
+            className="inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
+            onClick={handleClose}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M6 18L18 6M6 6l12 12"
-            ></path>
-          </svg>
-        </button>
+            Tamam
+          </button>
+          <button
+            type="button"
+            className="inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-gray-200 text-base font-medium text-gray-700 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:w-auto sm:text-sm"
+            onClick={handleClose}
+          >
+            İptal
+          </button>
+        </div>
       </div>
-      <div className="p-4">
-        <p>{desc}</p>
-      </div>
-      <div className="px-4 py-3 border-t border-gray-200 bg-gray-50 sm:px-6 sm:flex sm:flex-row-reverse">
-        <button
-          type="button"
-          className="inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
-          onClick={handleClose}
-        >
-          Tamam
-        </button>
-        <button
-          type="button"
-          className="inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-gray-200 text-base font-medium text-gray-700 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:w-auto sm:text-sm"
-          onClick={handleClose}
-        >
-          İptal
-        </button>
-      </div>
-    </div>
-  </div>, document.getElementById("modal")//başka yerde render etme işleminde portal kullandık
- )
+      <div
+        className=" bg-gray-800 bg-opacity-75 absolute w-full h-full top-0 left-0"
+        onClick={handleClose}
+      ></div>
+    </div>,
+    document.getElementById("modal")
+  );
 }
-
 
 Modal.propTypes = {
   setIsShowModal: PropTypes.func,
